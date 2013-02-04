781fd4976bef9a2401f5ecb7285f309b0679bfad
Christopher Petersen <christopher.petersen@gmail.com>
2013-02-03 21:29:39 -0800

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Journals::Application.config.secret_token = '5c5cc0ce0842db8e8329fc4ab585e52b78f1c40d1b60aab3986b96a201167f7ae71d9e09a24da316bbd6c328390d4c238c36cef2886f188b236ad24e2425723b'
